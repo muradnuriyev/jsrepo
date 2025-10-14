b04c09782932b7c7bd9fe56dc7339beb7689d7a1
Murad Nuriyev <mnuriyev101@gmail.com>
2025-10-14 21:11:27 +0400

    Lab 4: function (practice)

---
+++ calculateArea/calculate_amount.js
@@ -0,0 +1,12 @@
+let amount1;
+let amount2;
+let amount3;
+
+
+function calculateAmount() {
+    amount1 = parseFloat(document.getElementById("grocery1").value);
+    amount2 = parseFloat(document.getElementById("grocery2").value);
+    amount3 = parseFloat(document.getElementById("grocery3").value);
+    let totalAmount = amount1 + amount2 + amount3;
+    document.getElementById("result").innerText = "Total Amount: " + totalAmount;
+}
